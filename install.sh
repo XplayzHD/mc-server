21f3afaaf5d827ff7e0aea52511bdf485a11e33f
Apaullo11 <paul.bossley@gmail.com>
2019-12-16 17:08:50 -0500

    added backup capabilities

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@
 # this script can be downloaded and run via the following command:
 # curl -s https://raw.githubusercontent.com/bossley9/mc-server/master/install.sh | sudo bash
 
+#
+# 1. Setup startup server function
+#
+
 # download startup service file into system d directory
 sudo curl https://raw.githubusercontent.com/bossley9/mc-server/master/minecraftserver.service -o /etc/systemd/system/minecraftserver.service
 
@@ -19,11 +23,35 @@ while ! [[ $(sudo systemctl is-enabled minecraftserver) == "enabled" ]]; do
     sudo systemctl enable minecraftserver
 done
 
+#
+# 2. Install server executable
+#
+
 # download server execution file
 sudo curl https://raw.githubusercontent.com/bossley9/mc-server/master/mcserver.sh -o /usr/local/bin/mcserver.sh
 
 # change file permissions to allow executable
-sudo chmod 754 mcserver.sh
+sudo chmod 754 /usr/local/bin/mcserver.sh
+
+#
+# 3. Setup save file backups
+#
+
+# download saves backup file
+sudo curl https://raw.githubusercontent.com/bossley9/mc-server/master/mcserverbackup.sh -o /usr/local/bin/mcserverbackup.sh
+
+# change file permissions to allow executable
+sudo chmod 754 /usr/local/bin/mcserverbackup.sh
+
+# install anacron for weekly backups
+sudo apt-get install anacron
+
+# replaces default anacron file with modified version
+sudo curl https://raw.githubusercontent.com/bossley9/mc-server/master/anacrontab -o /etc/anacrontab
+
+#
+# 4. Start Server
+#
 
 # run server
 sudo systemctl start minecraftserver.sh
---
+++ mcserverbackup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# small script to manage minecraft server backups
+
+ROOT=/home/ubuntu/.mc-server/
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+
+# create time stamp folder and copies worlds into folder
+mkdir $ROOT/backup/$timestamp
+cp -r $ROOT/saves/* $ROOT/backup/$timestamp/
+
+# delete old worlds
+numDir=$(ls -l | grep -c ^d)
+if [[ $numDir >3 ]]
+	rm -r $(ls | head -n $((numDir-3)) )
+fi
