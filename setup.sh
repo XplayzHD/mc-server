749fad8df95e5bacf7a1956fcec1a44e84ff5d1a
sam bossley <35381535+bossley9@users.noreply.github.com>
2019-12-19 22:27:38 -0500

    prettified setup output

--- setup.sh
+++ setup.sh
@@ -19,21 +19,23 @@ EXECDIR="/usr/local/bin"
 # optimizing the environment
 #
 
-echo -e "${LB}optimizing the environment...${NC}"
+echo -e "${GN}optimizing the environment...${NC}"
 
-echo -e "\tdisabling bluetooth services..."
+echo -e "${LB}\tdisabling bluetooth services...${NC}"
 sudo systemctl stop bluetooth.service
 sudo systemctl disable bluetooth.service
 
 # https://www.instructables.com/id/Disable-the-Built-in-Sound-Card-of-Raspberry-Pi/
-echo -e "\tdisabling alsa sound..."
+echo -e "${LB}\tdisabling alsa sound...${NC}"
 blacklistAlsa="blacklist snd_bcm2835"
 alsaConf=/etc/modprobe.d/alsa-blacklist.conf
 grep -q "$blacklistAlsa" $alsaConf 2>/dev/null || echo "$blacklistAlsa" | sudo tee -a $alsaConf 1>/dev/null
 
 # https://www.cnx-software.com/2019/07/26/how-to-overclock-raspberry-pi-4/
 # https://hothardware.com/reviews/hot-clocked-pi-raspberry-pi-4-benchmarked-at-214-ghz
-echo -e "\tsetting up cpu overclock..."
+echo -e "${LB}\tsetting up cpu overclock...${NC}"
+# prevent prompting dialogs
+export APT_LISTCHANGES_FRONTEND=none
 yes | sudo apt-get update
 yes | sudo apt-get dist-upgrade
 # experimental releases for (possibly) better cpu clocking capacities
@@ -61,11 +63,11 @@ echo -e "${GN}done.${NC}"
 # creating startup service
 #
 
-echo -e "${LB}creating startup service...${NC}"
+echo -e "${GN}creating startup service...${NC}"
 
 sudo mkdir -p $SERVICEDIR
 
-echo -e "\tdownloading the startup service from the repository..."
+echo -e "${LB}\tdownloading the startup service from the repository...${NC}"
 # TODO update url
 sudo curl -s https://raw.githubusercontent.com/bossley9/mc-server/rework/minecraft.service -o $SERVICEDIR/minecraft.service
 sudo chmod 644 $SERVICEDIR/minecraft.service
@@ -73,7 +75,7 @@ sudo chmod 644 $SERVICEDIR/minecraft.service
 # reload daemon cache
 sudo systemctl daemon-reload
 
-echo -e "\tenabling the startup service..."
+echo -e "${LB}\tenabling the startup service...${NC}"
 while ! [[ $(sudo systemctl is-enabled minecraft) == "enabled" ]]; do
   sudo systemctl enable minecraft
 done
@@ -84,16 +86,16 @@ echo -e "${GN}done.${NC}"
 # setting up server directory
 #
 
-echo -e "${LB}setting up server directory...${NC}"
+echo -e "${GN}setting up server directory...${NC}"
 
-echo -e "\tcreating directory..."
+echo -e "${LB}\tcreating directory...${NC}"
 sudo mkdir -p $ROOTDIR
 sudo mkdir -p $EXECDIR/minecraft
 
-echo -e "\tsaving server directory path..."
+echo -e "${LB}\tsaving server directory path...${NC}"
 echo $ROOTDIR | sudo tee $EXECDIR/minecraft/rootpath.txt >/dev/null 2>&1
 
-echo -e "\tstoring endpoint url..."
+echo -e "${LB}\tstoring endpoint url...${NC}"
 echo -e "${YW}enter an endpoint url or press ENTER to continue with any previously entered url. This can always be updated later in $ROOTDIR/server.endpoint:${NC}"
 read endpoint
 if [[ ! -z $endpoint ]]; then
@@ -113,14 +115,14 @@ test -z "$worldName" && ! test -f $ROOTDIR/server.name && worldName="ServerWorld
 # updating server scripts 
 #
 
-echo -e "${LB}updating server scripts...${NC}"
+echo -e "${GN}updating server scripts...${NC}"
 
-echo -e "\tremoving old scripts..."
+echo -e "${LB}\tremoving old scripts...${NC}"
 sudo rm "$EXECDIR/minecraft/start.sh" >/dev/null 2>&1
 sudo rm "$EXECDIR/minecraft/stop.sh" >/dev/null 2>&1
 sudo rm "$EXECDIR/minecraft/restart.sh" >/dev/null 2>&1
 
-echo -e "\tretrieving new scripts..."
+echo -e "${LB}\tretrieving new scripts...${NC}"
 # TODO update urls
 sudo curl -s "https://raw.githubusercontent.com/bossley9/mc-server/rework/start.sh" -o "$EXECDIR/minecraft/start.sh"
 sudo chmod 754 "$EXECDIR/minecraft/start.sh"
@@ -135,28 +137,30 @@ echo -e "${GN}done.${NC}"
 # installing depedencies
 #
 
-echo -e "${LB}installing dependencies...${NC}"
+echo -e "${GN}installing dependencies...${NC}"
 yes | sudo apt-get update
 
-echo -e "\tinstalling java 8 jdk..."
+echo -e "${LB}\tinstalling java 8 jdk...${NC}"
 yes | sudo apt-get install openjdk-8-jre-headless
 if ! [ -n "`which java`" ]; then
   echo -e "${RD}java could not be installed correctly. Aborting.${NC}"
   exit 1
 fi
 
-echo -e "\tinstalling screen..."
+echo -e "${LB}\tinstalling screen...${NC}"
 yes | sudo apt-get install screen 
 if ! [ -n "`which screen`" ]; then
   echo -e "${RD}screen could not be installed correctly. Aborting.${NC}"
   exit 1
 fi
 
+echo -e "${GN}done.${NC}"
+
 #
 # configuring automatic reboot
 #
 
-echo -e "${LB}configuring automatic reboot...${NC}"
+echo -e "${GN}configuring automatic reboot...${NC}"
 
 echo -e "${LB}current system time is $(date)."
 echo -e "${YW}automatically reboot and update server at 4AM daily? This can always be changed via crontab -e. [Y/N]${NC}"
@@ -169,6 +173,8 @@ case $bAutoReboot in
     ;;
 esac
 
+echo -e "${GN}done.${NC}"
+
 #
 # system reboot
 #
@@ -179,6 +185,6 @@ case $bReboot in
   [Yy]*)
     sudo reboot
     ;;
-  *) echo -e "${LB}aborting setup.${NC}";
+  *) echo -e "${RD}aborting setup.${NC}";
 esac
 
